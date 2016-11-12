e4dc79c5c75b5b45d73ec785695bfad38d2c76c5
jeffxu <xujunyu2004@163.com>
2016-11-12 14:26:35 +0800

    delete file

--- IoT_MT7681_Panda/cust/auth/auth.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AUTH_H__
-#define __AUTH_H__
-
-char * getDeviceID()  XIP_ATTRIBUTE(".xipsec0");
-char * getVersionCode() XIP_ATTRIBUTE(".xipsec0");
-uint16 IoT_exec_AT_cmd_Serial(PUCHAR pCmdBuf, INT16 at_cmd_len) XIP_ATTRIBUTE(".xipsec0");
-
-#endif
